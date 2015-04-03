a4babe401de04dcc2276268535e76204698fe83c
alexliubj <alexliubo@gmail.com>
2015-04-03 12:09:58 -0400

    add questions.

--- public/modules/questions/controllers/questions.client.controller.js
+++ public/modules/questions/controllers/questions.client.controller.js
@@ -5,6 +5,49 @@ angular.module('questions').controller('QuestionsController', ['$scope', '$state
 	function($scope, $stateParams, $location, Authentication, Questions) {
 		$scope.authentication = Authentication;
 
+//template type
+ 		$scope.template = {
+        name: 'blue'
+      };
+      
+//numbers of questions 
+      $scope.numberOfQuestions = [
+      {name:'1', value:'1'},
+      {name:'2', value:'2'},
+      {name:'3', value:'3'},
+      {name:'4', value:'4'},
+      {name:'5', value:'5'},
+      {name:'6', value:'6'},
+      {name:'7', value:'7'},
+      {name:'8', value:'8'},
+      {name:'9', value:'9'},
+      {name:'10', value:'10'}
+    ];
+
+//choices.
+
+    var counter=0;
+    $scope.questionelemnt = [ {id:counter, question : 'Question-Click on me to edit!', answer : '',inline:true} ];
+
+    $scope.newItem = function($event){
+        counter++;
+        $scope.questionelemnt.push(  { id:counter, question : 'Question-Click on me to edit!', answer : '',inline:true} );
+        $event.preventDefault();
+    }
+    $scope.inlinef= function($event,inlinecontrol){
+        var checkbox = $event.target;
+        if(checkbox.checked){
+            $('#'+ inlinecontrol).css('display','inline');
+        }else{
+            $('#'+ inlinecontrol).css('display','');
+        }
+
+    }
+    $scope.showitems = function($event){
+        $('#displayitems').css('visibility','none');
+    }
+
+	//default
 		// Create new Question
 		$scope.create = function() {
 			// Create new Question object
