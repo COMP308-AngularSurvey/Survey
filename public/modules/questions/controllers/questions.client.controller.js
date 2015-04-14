a657eaeecd810c3b75b97ddbf4b447f321bafd7c
alexliubj <alexliubo@gmail.com>
2015-04-13 22:35:03 -0400

    resolv confilc

--- public/modules/questions/controllers/questions.client.controller.js
+++ public/modules/questions/controllers/questions.client.controller.js
@@ -143,14 +143,14 @@ angular.module('questions').controller('QuestionsController', ['$scope', '$state
 			// 	surveyId: '55209d8d4416313c18bceb20'
 			// });
 			$scope.questions = Questions.query();
-			for (var i in $scope.questions) {
+			for (var j in $scope.questions) {
 					
-						$scope.questionsWithId.push(($scope.questions[i])._id);
+						$scope.questionsWithId.push(($scope.questions[j])._id);
 				//	}
 				}
-				for(var j =0; j<=$scope.questions.length;j++)
+				for(var k =0; k<=$scope.questions.length;k++)
 				{
-					$scope.questionsWithId.push(($scope.questions[i])._id);
+					$scope.questionsWithId.push(($scope.questions[k])._id);
 				}
 			//$scope.questions = Questions.query(surveyId:$scope.testParameter);
 		};
