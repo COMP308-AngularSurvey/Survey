1d24d9c98e628f26d7f1f227b33052427ef9fed6
JohnnyHu9 <r17d09@gmail.com>
2015-04-17 00:22:45 -0400

    add survey list in home page

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -1,11 +1,33 @@
 'use strict';
 
 /**
+ * Mongoose dependencies.
+ */
+
+var mongoose = require('mongoose'),
+	errorHandler = require('./errors.server.controller'),
+	Survey = mongoose.model('Survey'),
+	_ = require('lodash');
+
+/**
  * Module dependencies.
  */
 exports.index = function(req, res) {
 	res.render('index', {
 		user: req.user || null,
 		request: req
+	});
+};
+
+
+exports.listActive = function(req,res){
+	Survey.find().sort('-created').exec(function(err, surveys) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.jsonp(surveys);
+		}
 	});
 };
--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,14 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', 'Surveys',
+	function($scope, Authentication,Surveys) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		// Find a active Surveys
+		$scope.find = function() {
+			$scope.surveys = Surveys.query();
+		};
 	}
 ]);
